bf5485350d8e1940b19e1849d11515a94966e13d
whdals6831 <whdals6831@gmail.com>
2021-06-23 22:05:33 +0900

    update path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./routes/Home";
 
 function App() {
   return <HashRouter>
-    <Route path="/" exact={true} component={Home} />
+    <Route path="/" basename={process.env.PUBLIC_URL} exact={true} component={Home} />
   </HashRouter>;
 }
 
